0e75dcd65c29e5e117d99df5b6efc28c5e373019
Itachi <annguyenvan.2k@gmail.com>
2021-06-01 16:26:26 +0700

    add courser dto

---
+++ src/main/java/com/example/student_management/converter/CourseConverter.java
@@ -0,0 +1,24 @@
+package com.example.student_management.converter;
+
+import com.example.student_management.domain.Class;
+import com.example.student_management.domain.Course;
+import com.example.student_management.dto.CourseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CourseConverter {
+    public CourseDto toDto(Course entity) {
+        List<String> classes = entity.getClasses().stream().map(Class::getName).collect(Collectors.toList());
+        CourseDto courseDto = CourseDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .type(entity.getType())
+                .createdDate(entity.getCreateDate())
+                .classes(classes)
+                .build();
+        return courseDto;
+    }
+}
---
+++ src/main/java/com/example/student_management/dto/CourseDto.java
@@ -0,0 +1,21 @@
+package com.example.student_management.dto;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CourseDto {
+    private Long id;
+    private String name;
+    private String type;
+    private Date createdDate;
+    private List<String> classes;
+
+}
