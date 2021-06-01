8a978daf6c762d07c0417ec8f4c89b1612eec92d
Itachi <annguyenvan.2k@gmail.com>
2021-06-01 14:57:51 +0700

    refactor name ExamResultRepository

--- src/main/java/com/example/student_management/repository/ExamResultRepository.java
+++ src/main/java/com/example/student_management/repository/ExamResultRepository.java
@@ -1,8 +1,9 @@
 package com.example.student_management.repository;
 
+import com.example.student_management.domain.ExamResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExamResult extends JpaRepository<ExamResult, Long> {
+public interface ExamResultRepository extends JpaRepository<ExamResult, Long> {
 }
