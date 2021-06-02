e20a8245d25bd0e43489bb7d88560895cccf0464
Itachi <annguyenvan.2k@gmail.com>
2021-06-02 14:47:32 +0700

    add exams converter

---
+++ src/main/java/com/example/student_management/converter/ExamConverter.java
@@ -0,0 +1,21 @@
+package com.example.student_management.converter;
+
+import com.example.student_management.domain.Exam;
+import com.example.student_management.domain.ExamResult;
+import com.example.student_management.dto.ExamDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ExamConverter {
+    public ExamDto toDto(Exam entity){
+
+        ExamDto examDto = ExamDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .course(entity.getCourse().getName())
+                .build();
+        return examDto;
+    }
+}
