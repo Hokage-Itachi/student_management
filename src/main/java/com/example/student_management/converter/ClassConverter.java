4c9a2e87b8a284a58df786f96ebbdbca6ebcdae8
Itachi <annguyenvan.2k@gmail.com>
2021-06-11 09:55:42 +0700

    add students information

--- src/main/java/com/example/student_management/converter/ClassConverter.java
+++ src/main/java/com/example/student_management/converter/ClassConverter.java
@@ -1,12 +1,10 @@
 package com.example.student_management.converter;
 
 import com.example.student_management.domain.Class;
-import com.example.student_management.domain.Event;
+import com.example.student_management.domain.Registration;
 import com.example.student_management.dto.ClassDto;
-import com.example.student_management.dto.CourseDto;
 import com.example.student_management.dto.EventDto;
-import com.example.student_management.dto.TeacherDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.student_management.service.StudentService;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -16,11 +14,11 @@ import java.util.stream.Collectors;
 @Component
 public class ClassConverter {
 
-    private TeacherConverter teacherConverter;
-    private EventConverter eventConverter;
+    private final StudentService studentService;
+    private final EventConverter eventConverter;
 
-    public ClassConverter(TeacherConverter teacherConverter, EventConverter eventConverter) {
-        this.teacherConverter = teacherConverter;
+    public ClassConverter(StudentService studentService, EventConverter eventConverter) {
+        this.studentService = studentService;
         this.eventConverter = eventConverter;
     }
 
@@ -29,7 +27,13 @@ public class ClassConverter {
         if (entity.getEvents() != null) {
             events = entity.getEvents().stream().map(eventConverter::toDto).collect(Collectors.toList());
         }
-        ClassDto classDto = ClassDto.builder()
+        List<Registration> registrations = entity.getRegistrations();
+        List<String> students = new ArrayList<>();
+        if (registrations != null) {
+            students = registrations.stream().map(registrationId -> studentService.findById(registrationId.getId().getStudentId()).get().getFullName()).collect(Collectors.toList());
+
+        }
+        return ClassDto.builder()
                 .id(entity.getId())
                 .name(entity.getName())
                 .startDate(entity.getStartDate())
@@ -38,8 +42,8 @@ public class ClassConverter {
                 .course(entity.getCourse().getName())
                 .teacher(entity.getTeacher().getFullName())
                 .events(events)
+                .students(students)
                 .build();
-        return classDto;
     }
 
     public Class toEntity(ClassDto classDto) {
