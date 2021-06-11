b411bbcf2de0ff4708ad8e0fd065bde20eeffe51
Itachi <annguyenvan.2k@gmail.com>
2021-06-11 09:55:32 +0700

    add classes information

--- src/main/java/com/example/student_management/converter/StudentConverter.java
+++ src/main/java/com/example/student_management/converter/StudentConverter.java
@@ -1,16 +1,29 @@
 package com.example.student_management.converter;
 
-import com.example.student_management.domain.Class;
+import com.example.student_management.domain.Registration;
 import com.example.student_management.domain.Student;
 import com.example.student_management.dto.StudentDto;
+import com.example.student_management.service.ClassService;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class StudentConverter {
+    private final ClassService classService;
+
+    public StudentConverter(ClassService classService) {
+        this.classService = classService;
+    }
+
     public StudentDto toDto(Student entity) {
+        List<Registration> registrations = entity.getRegistrations();
+        List<String> classes = new ArrayList<>();
+        if (registrations != null) {
+            classes = registrations.stream().map(registrationId -> classService.findById(registrationId.getId().getClassId()).get().getName()).collect(Collectors.toList());
+        }
         return StudentDto.builder()
                 .id(entity.getId())
                 .fullName(entity.getFullName())
@@ -21,6 +34,7 @@ public class StudentConverter {
                 .note(entity.getNote())
                 .facebook(entity.getFacebook())
                 .createDate(entity.getCreateDate())
+                .classes(classes)
                 .build();
     }
 
