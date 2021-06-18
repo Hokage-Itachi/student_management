f25e13e2c23adba4cef59bd3e0f3906552f65ec4
Itachi <annguyenvan.2k@gmail.com>
2021-06-18 15:14:09 +0700

    add CustomUserDetails

---
+++ src/main/java/com/example/student_management/security/auth/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.example.student_management.security.auth;
+
+import com.example.student_management.domain.Permission;
+import com.example.student_management.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<Permission> permissionsObjs = user.getPermissions();
+        if (permissionsObjs == null || permissionsObjs.isEmpty()) {
+            return new ArrayList<SimpleGrantedAuthority>();
+        }
+        List<String> permissions = permissionsObjs.stream().map(Permission::getPerName).collect(Collectors.toList());
+        return permissions.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
