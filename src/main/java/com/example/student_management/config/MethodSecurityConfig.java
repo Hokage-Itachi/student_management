6e8751ef7c59fdca621bc1e20dbc942e1fea2d17
Itachi <annguyenvan.2k@gmail.com>
2021-06-22 15:21:04 +0700

    refactor code

--- src/main/java/com/example/student_management/config/MethodSecurityConfig.java
+++ src/main/java/com/example/student_management/config/MethodSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.example.student_management.config;
 
 import com.example.student_management.security.CustomMethodSecurityExpressionHandler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
@@ -14,7 +12,6 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
     @Override
     protected MethodSecurityExpressionHandler createExpressionHandler() {
-        final DefaultMethodSecurityExpressionHandler expressionHandler = new CustomMethodSecurityExpressionHandler();
-        return expressionHandler;
+        return new CustomMethodSecurityExpressionHandler();
     }
 }
