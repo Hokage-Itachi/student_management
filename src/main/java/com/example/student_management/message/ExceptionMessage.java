810b9137ba67cf2fcf56ac49c950bcd675fc6264
Itachi <annguyenvan.2k@gmail.com>
2021-06-23 17:14:21 +0700

    add exception message enum

---
+++ src/main/java/com/example/student_management/message/ExceptionMessage.java
@@ -0,0 +1,30 @@
+package com.example.student_management.message;
+
+public enum ExceptionMessage {
+    CLASS_NOT_FOUND("Class with id %d not found"),
+    COURSE_NOT_FOUND("Course with id %d not found"),
+    EVENT_NOT_FOUND("Event with id %d not found"),
+    EXAM_NOT_FOUND("Exam with id %d not found"),
+    EXAM_RESULT_NOT_FOUND("Exam result with id %d not found"),
+    PERMISSION_NOT_FOUND("Permission with id %d not found"),
+    PLAN_NOT_FOUND("Plan with id %d not found"),
+    REGISTRATION_NOT_FOUND("Registration with id %d not found"),
+    ROLE_NOT_FOUND("Role with name '%s' not found"),
+    STUDENT_NOT_FOUND("Student with id %d not found"),
+    TEACHER_NOT_FOUND_BY_ID("Teacher with id %d not found"),
+    TEACHER_NOT_FOUND_BY_EMAIL("Teacher with email '%s' not found"),
+    USER_NOT_FOUND_BY_ID("User with id %d not found"),
+    USER_NOT_FOUND_BY_EMAIL("User with email '%s' not found"),
+    USER_NOT_FOUND_BY_USERNAME("User with username '%s' not found");
+
+    public final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
+}
