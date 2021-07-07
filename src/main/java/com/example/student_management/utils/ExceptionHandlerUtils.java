299d53fe6597b13d2a4e899dc9e407f51a7177f0
Itachi <annguyenvan.2k@gmail.com>
2021-07-07 14:20:13 +0700

    update response data

--- src/main/java/com/example/student_management/utils/ExceptionHandlerUtils.java
+++ src/main/java/com/example/student_management/utils/ExceptionHandlerUtils.java
@@ -8,7 +8,6 @@ public class ExceptionHandlerUtils {
     public static Map<String, Object> createResponseData(String error, Integer status, String message) {
         Map<String, Object> data = new LinkedHashMap<>();
         data.put("timestamp", new Timestamp(System.currentTimeMillis()));
-        data.put("status", status);
         data.put("error", error);
         data.put("message", message);
         return data;
