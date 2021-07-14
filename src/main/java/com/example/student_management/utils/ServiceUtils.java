e6bce765b6f92f1aef6e202f0bc398adf48d9f1c
Itachi <annguyenvan.2k@gmail.com>
2021-07-14 13:32:31 +0700

    add getNullProperty function

--- src/main/java/com/example/student_management/utils/ServiceUtils.java
+++ src/main/java/com/example/student_management/utils/ServiceUtils.java
@@ -1,5 +1,10 @@
 package com.example.student_management.utils;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,5 +32,17 @@ public class ServiceUtils {
         String message = arr[arr.length - 1];
         message = message + " can not be null";
         return message;
+    }
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
